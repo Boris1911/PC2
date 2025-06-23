8bc4589044746cd88c01f3ce653425d1b42c56bc
Boris <22101523@ue.edu.pe>
2025-06-23 11:57:36 -0500

    Primer commit

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,6 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex flex-center" style="height: 100vh;">
+    <!-- Mensaje eliminado -->
   </q-page>
 </template>
 
